1a0f247411aae5333f60bfd375a3608c9745a0be
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-07-22 17:01:39 +0500

    update tasks' index

--- src/module/clearTask.js
+++ src/module/clearTask.js
@@ -2,6 +2,15 @@ import { tasks, storeTasksToLocalStorage } from './taskFunctions.js';
 
 const clearAllCompletedTasks = () => {
   const filteredTasks = tasks.filter((task) => !task.completed);
+
+  // Sort the filtered tasks based on original indexes
+  filteredTasks.sort((a, b) => a.index - b.index);
+
+  // Update the indexes starting from 1
+  filteredTasks.forEach((task, newIndex) => {
+    task.index = newIndex + 1;
+  });
+
   tasks.length = 0;
   tasks.push(...filteredTasks);
   storeTasksToLocalStorage();
